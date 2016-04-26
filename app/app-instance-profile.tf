a7285882be95423e0c03ad1d3f6da46dd290a7ed
Richard Knop <risoknop@gmail.com>
2016-04-26 23:33:47 +0800

    Added S3 policy to app module so it can download release tarball from S3.

--- app/app-instance-profile.tf
+++ app/app-instance-profile.tf
@@ -7,6 +7,16 @@ resource "aws_iam_role" "app" {
   assume_role_policy = "${template_file.app_assume_role_policy.rendered}"
 }
 
+resource "template_file" "app_s3_policy" {
+  template = "${file("${path.module}/templates/app-s3-policy.json")}"
+}
+
+resource "aws_iam_role_policy" "s3" {
+  name = "${var.env}-app-iam-role-s3-policy"
+  role = "${aws_iam_role.app.id}"
+  policy = "${template_file.app_s3_policy.rendered}"
+}
+
 resource "aws_iam_instance_profile" "app" {
   name = "${var.env}-app-iam-profile"
   roles = ["${aws_iam_role.app.name}"]
