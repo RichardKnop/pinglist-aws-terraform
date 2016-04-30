40bacdf79cd57b58a5ebc33c0763a645da8ae51a
Richard Knop <risoknop@gmail.com>
2016-04-30 20:25:22 +0800

    Added S3 bucket for assets.

--- s3/s3.tf
+++ s3/s3.tf
@@ -8,3 +8,23 @@ resource "aws_vpc_endpoint" "private_s3" {
   policy = "${template_file.private_s3_policy.rendered}"
   route_table_ids = ["${var.private_route_table}"]
 }
+
+resource "template_file" "s3_assets_bucket_policy" {
+  template = "${file("${path.module}/templates/s3-assets-bucket-policy.json")}"
+
+  vars {
+    env = "${var.env}"
+  }
+}
+
+resource "aws_s3_bucket" "assets" {
+  bucket = "${var.env}.pinglist.assets"
+  acl = "private"
+  force_destroy = "${var.force_destroy}"
+  policy = "${template_file.s3_assets_bucket_policy.rendered}"
+
+  tags {
+    Name = "S3 bucket for assets"
+    Environment = "${var.env}"
+  }
+}
