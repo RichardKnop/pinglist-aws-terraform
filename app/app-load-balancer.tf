43195d061c16f9bf4d2925402f9be5cc621c3598
Richard Knop <risoknop@gmail.com>
2016-05-19 14:44:46 +0800

    Allowed HTTP traffic to the web app to allow redirection to HTTPS.

--- app/app-load-balancer.tf
+++ app/app-load-balancer.tf
@@ -8,6 +8,13 @@ resource "aws_elb" "app" {
   listener {
     instance_port = 80
     instance_protocol = "http"
+    lb_port = 80
+    lb_protocol = "http"
+  }
+
+  listener {
+    instance_port = 80
+    instance_protocol = "http"
     lb_port = 443
     lb_protocol = "https"
     ssl_certificate_id = "${var.ssl_certificate_id}"
